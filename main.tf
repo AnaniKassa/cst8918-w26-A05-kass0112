be69fce062b90d53e0234bd996127cb537e768a5
AnaniKassa <kass0112@algonquinlive.com>
2026-02-28 15:15:31 -0500

    last commit

--- main.tf
+++ main.tf
@@ -24,3 +24,232 @@ provider "azurerm" {
 provider "cloudinit" {
   # Configuration options
 }
+
+# add variables
+variable "labelPrefix" {}
+
+variable "region" {
+  default = "Canada Central"
+}
+
+variable "admin_username" {
+  default = "azureadmin"
+}
+
+# add resources 
+resource "azurerm_resource_group" "rg" {
+
+  name = "${var.labelPrefix}-A05-RG"
+
+  location = var.region
+}
+
+
+# add Public IP Address
+resource "azurerm_public_ip" "pip" {
+
+  name = "${var.labelPrefix}-pip"
+
+  location = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+
+  sku = "Standard"
+}
+
+# add Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+
+  name = "${var.labelPrefix}-vnet"
+
+  location = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = ["10.0.0.0/16"]
+}
+
+# add Subnet
+resource "azurerm_subnet" "subnet" {
+
+  name = "web-subnet"
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.1.0/24"]
+
+}
+
+# add Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+
+  name = "${var.labelPrefix}-nsg"
+
+  location = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+
+    name = "SSH"
+
+    priority = 1001
+
+    direction = "Inbound"
+
+    access = "Allow"
+
+    protocol = "Tcp"
+
+    source_port_range = "*"
+
+    destination_port_range = "22"
+
+    source_address_prefix = "*"
+
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+
+    name = "HTTP"
+
+    priority = 1002
+
+    direction = "Inbound"
+
+    access = "Allow"
+
+    protocol = "Tcp"
+
+    source_port_range = "*"
+
+    destination_port_range = "80"
+
+    source_address_prefix = "*"
+
+    destination_address_prefix = "*"
+  }
+
+}
+
+# add Virtual Network Interface Card (NIC)
+resource "azurerm_network_interface" "nic" {
+
+  name = "${var.labelPrefix}-nic"
+
+  location = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+
+    name = "internal"
+
+    subnet_id = azurerm_subnet.subnet.id
+
+    private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = azurerm_public_ip.pip.id
+
+  }
+
+}
+
+# Apply NSG to NIC
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+
+  network_interface_id = azurerm_network_interface.nic.id
+
+  network_security_group_id = azurerm_network_security_group.nsg.id
+
+}
+
+# add Cloud Init 
+
+data "cloudinit_config" "init" {
+
+  gzip = false
+
+  base64_encode = false
+
+  part {
+
+    filename = "init.sh"
+
+    content_type = "text/x-shellscript"
+
+    content = file("init.sh")
+
+  }
+
+}
+
+# add Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+
+  name = "${var.labelPrefix}-vm"
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location = azurerm_resource_group.rg.location
+
+  size = "Standard_B1s"
+
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+
+    username = var.admin_username
+
+    public_key = file("/home/terry/.ssh/id_rsa.pub")
+
+  }
+
+  os_disk {
+
+    caching = "ReadWrite"
+
+    storage_account_type = "Standard_LRS"
+
+  }
+
+  source_image_reference {
+
+    publisher = "Canonical"
+
+    offer = "0001-com-ubuntu-server-jammy"
+
+    sku = "22_04-lts"
+
+    version = "latest"
+
+  }
+
+  custom_data = base64encode(
+    data.cloudinit_config.init.rendered
+  )
+
+}
+
+# add Outputs
+output "resource_group_name" {
+
+  value = azurerm_resource_group.rg.name
+
+}
+
+output "public_ip" {
+
+  value = azurerm_public_ip.pip.ip_address
+
+}
